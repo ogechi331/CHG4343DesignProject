01e7df49eb25f0cfd667e4273ad65a55d5504113
alexm <amcdo079@uottawa.ca>
2023-11-03 15:46:07 -0400

    Saving project

--- src/CSTRReactor.java
+++ src/CSTRReactor.java
@@ -1,63 +1,55 @@
 public class CSTRReactor extends Reactor {
-    //takes reaction rate and updates C and flowrate TimeStepReaction
-    //Gives controlled ot PID and takes in commanded
-    private double setPoint;
-    //intensive varialbes
     private double V;
-    private  double v0;
-    private double[] concentrations; //setEqualtoA&B
-    private double[] flowConcentrations;
-    private double flowRate;//assuming ideal mixing and constant level
-    private Species[] species;
-    private Species[] reactants;
-    private Species[] products;
+    private Reaction reaction;
 
-    private double timeStep;
-    private Reaction zeroOrderReaction;
+    private double[] inletConcentrations;
 
-    public CSTRReactor(double setPoint,double V,double v0, Reaction zeroOrderReaction, double timeStep, double flowRate,
-                       double flowConcentrations[],double concentrations[]){
+    public CSTRReactor(double setPoint,double V, Reaction reaction
+                       double inletConcentrations[]){
         this.setPoint=setPoint;
         this.V=V;
-        this.v0=v0;
-        this.zeroOrderReaction=new Reaction(zeroOrderReaction);
-        this.timeStep=timeStep;
-        this.flowRate-flowRate;
-        this.reactants= zeroOrderReaction.getReactants();
-        this.products= zeroOrderReaction.getProducts();
-        for(int i;i=0;i<flowConcentrations.length){
-            this.flowConcentrations[i] = flowConcentrations[i];
+        this.reaction=new Reaction(reaction);
+        for(int i;i=0;i<inletConcentrations.length){
+            this.inletConcentrations[i] = inletConcentrations[i];
         }
-        this.flowRate=flowRate;
-        for(int i;i=0;i<(this.reactants.length+this.products.length)){
-            if(i<this.reactants.length) {
-                species[i] = new Species(this.reactants[i]);
-            }
-            else {
-                species[i] = new Species(this.reactants[i]);
-            }
-        }
-        this.flowConcentrations.clone();
-        this.concentrations=concentrations.clone();
-
     }
-    public boolean equals(Object comparator){
+
+    public double getV() {
+        return V;
+    }
+
+    public void setV(double v) {
+        V = v;
+    }
+
+    public double[] getInletConcentrations() {
+        return inletConcentrations;
+    }
+
+    public void setInletConcentrations(double[] inletConcentrations) {
+        this.inletConcentrations = inletConcentrations;
+    }
+
+    public Reaction getReaction() {
+        return reaction;
+    }
+
+    public void setReaction(Reaction reaction) {
+        this.reaction = reaction;
+    }
+
+
+    public boolean equals(Object comparator){ //fix this
         if comparator==null return false;
         else return (this.getClass()==comparator.getClass());
     }
-    public double simulateStep(double commandedVariable){
-        flowRate=commandedVariable;
-        private double k;
-        k=zeroOrderReaction.calculateReactionRate(concentrations);
-        for(int i;i=0;i<concentrations.length){
-            concentrations[i] += concentrations[i] * k *timeStep*species[i].getCoefficient();
-            concentrations[i] += flowConcentrations[i] * timeStep*flowRate/V;//inflow
-            concentrations[i] -= concentrations[i] *timeStep*flowRate/V;//outflow
-        }
-        return controlledVariable;
+    public double simulateStep(double timeStep,double flowRate, double[] currentConcentrations, double currentSpeciesNumber){
+        double reactionRate;
+        double updatedConcentration;
+        reactionRate=reaction.calculateReactionRate(currentConcentrations,currentSpeciesNumber);
+        updatedConcentration=currentConcentrations[currentSpeciesNumber]+timeStep*((flowRate/this.V)*
+                inletConcentrations[currentSpeciesNumber]-currentConcentrations[currentSpeciesNumber]*flowRate/this.V+
+                currentConcentrations[currentSpeciesNumber]*reactionRate);
+        return updatedConcentration;
     }
-
-
-
-
 }
