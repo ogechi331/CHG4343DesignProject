fa39fc4f951364c6fbc9ad5936b450f13d210543
dylanlaytonmatthews <dlayt096@uottawa.ca>
2023-12-07 16:01:42 -0500

    Working on testing driver file for validation of methods - Dylan

--- src/TestingDriver.java
+++ src/TestingDriver.java
@@ -5,6 +5,11 @@
  */
 public class TestingDriver {
 
+    /** Main method which does nothing by default
+     * Can uncomment lines to run the methods used in the validation procedure
+     * @param args
+     * @author Dylan
+     */
     public static void main(String args[]) {
 
         //Comment and uncomment to test
@@ -14,9 +19,12 @@ public class TestingDriver {
         //testReaction();
         //uncomment next line to test species class
         //testSpecies();
+        //uncomment next line to test the PID controller class
+        //testPIDController();
 
 
 
+        //RK45 has its own validation driver using an IVP with an analytical solution
         //interfaces are implemented and not testable on their own
         //Queue and SinglyLinkedList are tested when in use with controller and generic
 
@@ -24,7 +32,7 @@ public class TestingDriver {
     }
 
     /** Static method to test methods in the Reactor and CSTRReactor Classes
-     * @author Dyla
+     * @author Dylan
      */
         public static void testReactorClass() {
 
@@ -263,6 +271,231 @@ public class TestingDriver {
         //other methods are tested whenever the controller is used
 
     } //end of species checks
+
+    /** Static method to test methods in the PIDController Class
+     * @author Dylan
+     */
+    public static void testPIDController () {
+
+        double volume = 1;
+        double initialFlow = 0.05;
+        double k = 0.2;
+        String reactionString = "A->B";
+        Reaction reaction = new Reaction(reactionString, k);
+        double[] initialConcentrations = {0.04, 0.16};
+        double[] inletConcentrations = {1.2, 0};
+        int controlled = 0;
+        boolean isControlled = false;
+        double[] out;
+
+        //valid reactor
+        CSTRReactor CSTR = new CSTRReactor(volume, initialFlow, reaction, initialConcentrations, inletConcentrations, controlled, isControlled);
+
+        double startTime = 0;
+        double endTime = 10;
+        double timeStep = 1;
+        double controllerGain = 1;
+        double integratingTimeConstant = 1;
+        double derivativeTimeConstant = 1;
+        PIDController.CONTROLLER_TYPE controllerType = PIDController.CONTROLLER_TYPE.PID;
+        double deadTime = 0;
+        Controllable controllable = CSTR;
+        double tolerance = 0.1;
+        Queue<double[]> disturbances = new Queue<>();
+        String string = "1.2, 0";
+        String[] s = string.trim().split(",");
+        double[] d = new double[2];
+        for(int i = 0; i < 2; i++){
+            d[i] = Double.parseDouble(s[i].trim());
+        }
+        disturbances.enqueue(d);
+        double setPoint = 0.04;
+
+        //valid controller test
+
+        try { //unchecked exception so this is needed
+
+            PIDController PID = new PIDController(startTime, endTime, timeStep, controllerGain, integratingTimeConstant, derivativeTimeConstant, controllerType, deadTime, controllable, tolerance, disturbances, setPoint);
+
+            System.out.println("start time 0->0.1 test");
+            System.out.println(PID.getStartTime());
+            PID.setStartTime(0.1);
+            System.out.println(PID.getStartTime());
+
+            System.out.println("invalid start time 0.1->100 test");
+            System.out.println(PID.getStartTime());
+            PID.setStartTime(100);
+            System.out.println(PID.getStartTime());
+
+            System.out.println("end time 10->10.1 test");
+            System.out.println(PID.getEndTime());
+            PID.setEndTime(10.1);
+            System.out.println(PID.getEndTime());
+
+            System.out.println("invalid end time 10.1->0 test");
+            System.out.println(PID.getEndTime());
+            PID.setEndTime(0);
+            System.out.println(PID.getEndTime());
+
+
+            System.out.println("timestep 1->0.1 test");
+            System.out.println(PID.getTimeStep());
+            PID.setTimeStep(0.1);
+            System.out.println(PID.getTimeStep());
+
+            System.out.println("invalid timestep 0.1->100 test");
+            System.out.println(PID.getTimeStep());
+            PID.setTimeStep(100);
+            System.out.println(PID.getTimeStep());
+
+            System.out.println("controller gain 1->1.1 test");
+            System.out.println(PID.getControllerGain());
+            PID.setControllerGain(1.1);
+            System.out.println(PID.getControllerGain());
+
+            System.out.println("invalid controller gain 1.1->-1 test");
+            System.out.println(PID.getControllerGain());
+            PID.setControllerGain(-1);
+            System.out.println(PID.getControllerGain());
+
+            System.out.println("controller tau I 1->1.1 test");
+            System.out.println(PID.getIntegratingTimeConstant());
+            PID.setIntegratingTimeConstant(1.1);
+            System.out.println(PID.getIntegratingTimeConstant());
+
+            System.out.println("invalid tau I 1.1->-1 test");
+            System.out.println(PID.getIntegratingTimeConstant());
+            PID.setIntegratingTimeConstant(-1);
+            System.out.println(PID.getIntegratingTimeConstant());
+
+            System.out.println("tau D 1->1.1 test");
+            System.out.println(PID.getDerivativeTimeConstant());
+            PID.setDerivativeTimeConstant(1.1);
+            System.out.println(PID.getDerivativeTimeConstant());
+
+            System.out.println("invalid controller gain 1.1->-1 test");
+            System.out.println(PID.getDerivativeTimeConstant());
+            PID.setDerivativeTimeConstant(-1);
+            System.out.println(PID.getDerivativeTimeConstant());
+
+            System.out.println("controller type PID->PI test");
+            System.out.println(PID.getControllerType());
+            PID.setControllerType(PIDController.CONTROLLER_TYPE.PI);
+            System.out.println(PID.getControllerType());
+
+            System.out.println("invalid controller type PID->null test");
+            System.out.println(PID.getControllerType());
+            PID.setControllerType(null);
+            System.out.println(PID.getControllerType());
+
+            System.out.println("dead time 0->1 test");
+            System.out.println(PID.getDeadTime());
+            PID.setDeadTime(1);
+            System.out.println(PID.getDeadTime());
+
+            System.out.println("invalid dead time 1->-1 test");
+            System.out.println(PID.getDeadTime());
+            PID.setDeadTime(-1);
+            System.out.println(PID.getDeadTime());
+
+            System.out.println("tolerance 0.1->0.05 test");
+            System.out.println(PID.getTolerance());
+            PID.setTolerance(0.05);
+            System.out.println(PID.getTolerance());
+
+            System.out.println("invalid tolerance 0.05->-1 test");
+            System.out.println(PID.getTolerance());
+            PID.setTolerance(-1);
+            System.out.println(PID.getTolerance());
+
+            System.out.println("set point 0.04->0.08 test");
+            System.out.println(PID.getSetPoint());
+            PID.setSetPoint(0.08);
+            System.out.println(PID.getSetPoint());
+
+            PIDController PID_2 = new PIDController(startTime, endTime, timeStep, controllerGain, integratingTimeConstant, derivativeTimeConstant, controllerType, deadTime, controllable, tolerance, disturbances, setPoint);
+
+            System.out.println("Test of true equals method and clone method");
+
+            PIDController PID_3 = PID_2.clone();
+
+            System.out.println(PID_2.equals(PID_3));
+
+            System.out.println("Test of false equals method");
+            PIDController PID_4 = new PIDController(PID_2);
+            PID_4.setSetPoint(100);
+            System.out.println(PID_2.equals(PID_4));
+
+            System.out.println("Test of invalid copy of PID Controller");
+            try {
+                PIDController PID_5 = new PIDController(null);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+
+            System.out.println("Test of invalid constructor, start time");
+            try {
+                PIDController PID_5 = new PIDController(11, endTime, timeStep, controllerGain, integratingTimeConstant, derivativeTimeConstant, controllerType, deadTime, controllable, tolerance, disturbances, setPoint);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+            System.out.println("Test of invalid constructor, end time");
+            try {
+                PIDController PID_5 = new PIDController(startTime, 0, timeStep, controllerGain, integratingTimeConstant, derivativeTimeConstant, controllerType, deadTime, controllable, tolerance, disturbances, setPoint);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+            System.out.println("Test of invalid constructor, time step");
+            try {
+                PIDController PID_5 = new PIDController(startTime, endTime, 100, controllerGain, integratingTimeConstant, derivativeTimeConstant, controllerType, deadTime, controllable, tolerance, disturbances, setPoint);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+            System.out.println("Test of invalid constructor, KC");
+            try {
+                PIDController PID_5 = new PIDController(startTime, endTime, timeStep, -1, integratingTimeConstant, derivativeTimeConstant, controllerType, deadTime, controllable, tolerance, disturbances, setPoint);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+            System.out.println("Test of invalid constructor, Tau I");
+            try {
+                PIDController PID_5 = new PIDController(startTime, endTime, timeStep, controllerGain, -1, derivativeTimeConstant, controllerType, deadTime, controllable, tolerance, disturbances, setPoint);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+            System.out.println("Test of invalid constructor, Tau D");
+            try {
+                PIDController PID_5 = new PIDController(startTime, endTime, timeStep, controllerGain, integratingTimeConstant, -1, controllerType, deadTime, controllable, tolerance, disturbances, setPoint);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+            System.out.println("Test of invalid constructor, controller type");
+            try {
+                PIDController PID_5 = new PIDController(startTime, endTime, timeStep, controllerGain, integratingTimeConstant, derivativeTimeConstant, null, deadTime, controllable, tolerance, disturbances, setPoint);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+            System.out.println("Test of invalid constructor, dead time");
+            try {
+                PIDController PID_5 = new PIDController(startTime, endTime, timeStep, controllerGain, integratingTimeConstant, derivativeTimeConstant, controllerType, -1, controllable, tolerance, disturbances, setPoint);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+            System.out.println("Test of invalid constructor, tolerance");
+            try {
+                PIDController PID_5 = new PIDController(startTime, endTime, timeStep, controllerGain, integratingTimeConstant, derivativeTimeConstant, controllerType, deadTime, controllable, -1, disturbances, setPoint);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+
+            //other methods tested in test cases to get valid controller outputs
+
+    }
+        catch (CloneNotSupportedException e) {
+        System.out.println(e.getMessage());
+    }
+
+    } //end of PID controller checks
 
 
 
